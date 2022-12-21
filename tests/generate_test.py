8c51e7efead8f5dd8c4c02036279fc6c296b0c5d
Joao Gouveia <jjtucki4@gmail.com>
2022-12-21 22:51:02 +0000

    Fixe test generation

--- tests/generate_test.py
+++ tests/generate_test.py
@@ -4,8 +4,8 @@ with open("generated_test.in", "w") as test:
     test.write(str(vertices) + "\n")
     test.write(str(vertices*(vertices-1)) + "\n")
 
-    for i in range(vertices):
-        for j in range(vertices):
+    for i in range(1, vertices + 1):
+        for j in range(1, vertices + 1):
             if (j != i):
                 test.write(str(i) + " " + str(j) + " 1\n")
 
