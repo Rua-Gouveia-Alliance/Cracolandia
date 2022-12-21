79bbee2a0c2abc766c0b557de08ca0721998e8ee
Joao Gouveia <jjtucki4@gmail.com>
2022-12-21 20:56:56 +0000

    Extra comments

--- src/p2.cpp
+++ src/p2.cpp
@@ -42,7 +42,7 @@ bool doesnt_contain(std::vector<int> vec, int i) {
     return true;
 }
 
-// Prim's algorithm variation
+// Prim's algorithm variation, we want the most expensive tree, not the least expensive one
 unsigned long int get_maximum_cost_spanning_tree(Graph* graph, std::vector<int> considered_vertices) {
     int vertex_count = considered_vertices.size(), edge_count = vertex_count - 1;
     std::vector<bool> in_queue(graph->vertex_count, true);
@@ -80,7 +80,7 @@ std::vector<int> dfs(Graph* graph, int source) {
     return stack;
 }
 
-// Applying the algorithm relying on dfs to get the graph sccs
+// Applying the algorithm relying on dfs to get the graph's sccs
 std::vector<std::vector<int>> get_sccs(Graph* graph) {
     std::vector<std::vector<int>> sccs;
     std::vector<int> stack = dfs(graph, 0);
