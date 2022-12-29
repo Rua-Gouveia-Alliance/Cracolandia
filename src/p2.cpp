af6df223d74ed1e2de4039a2331fd6ea673de300
Joao Gouveia <jjtucki4@gmail.com>
2022-12-29 03:50:39 +0000

    Kruskal algorithm implementation, still doesnt work if there is more than 1 scc

--- src/p2.cpp
+++ src/p2.cpp
@@ -5,124 +5,139 @@
 #define VISITING 1
 #define NOTVISITED 2
 
-typedef struct {
+#define LOG(x) std::cout << x << std::endl
+
+struct tree_node_t {
+    tree_node_t* p;
+    size_t rank;
     int id;
+};
+
+struct edge_t {
+    tree_node_t* u;
+    tree_node_t* v;
     size_t weight;
-} Vertex;
+};
 
-typedef struct {
-    size_t vertex_count;
-    std::vector<std::vector<Vertex>> adjacent_vertices;
-} Graph;
+struct heap_t {
+    int size;
+    std::vector<edge_t> edges;
+};
 
-unsigned long int get_tree_cost(std::vector<size_t> &key) {
-    unsigned long int cost = 0;
-    for (const auto & w : key)
-        cost += w;
-    return cost;
+static inline int parent(int i) {
+    return i >> 1;
 }
 
-int get_maximum_edge(std::vector<size_t> &key, std::vector<bool> &in_queue, std::vector<int> &considered_vertices) {
-    int max = 0;
-    for (const auto & i : considered_vertices) {
-        if (in_queue[i] && key[i] > key[max]) 
-            max = i;
-        else if (!in_queue[max] && in_queue[i])
-            max = i;
-    }
-    return max;
+static inline int left(int i) {
+    return i << 1;
 }
 
-bool doesnt_contain(std::vector<int> &vec, int i) {
-    for (const auto & n : vec)
-        if (i == n)
-            return false;
-    return true;
+static inline int right(int i) {
+    return (i << 1) + 1;
 }
 
-// Prim's algorithm variation, we want the most expensive spanning tree, not the least expensive one
-unsigned long int get_maximum_cost_spanning_tree(Graph* graph, std::vector<int> &considered_vertices) {
-    int edge_count = considered_vertices.size() - 1;
-    std::vector<bool> in_queue(graph->vertex_count, true);
-    std::vector<size_t> key(graph->vertex_count, 0);
-
-    for (size_t i = 0; i < graph->vertex_count; i++)
-        if (doesnt_contain(considered_vertices, i))
-            in_queue[i] = false;
-
-    for (int tree_size = 0; tree_size < edge_count; tree_size++) {
-        int u = get_maximum_edge(key, in_queue, considered_vertices);
-        in_queue[u] = false;
-        for (const auto & v : graph->adjacent_vertices[u])
-            if (in_queue[v.id] && v.weight > key[v.id])
-                key[v.id] = v.weight;
-    }
-    return get_tree_cost(key);
+void swap(heap_t* heap, int i, int j) {
+    edge_t aux = heap->edges[i];
+    heap->edges[i] = heap->edges[j];
+    heap->edges[j] = aux;
 }
 
-void visit(Graph* graph, int vertex, std::vector<int> &stack, std::vector<int> &status) {
-    status[vertex] = VISITING;
-    for (const auto & v : graph->adjacent_vertices[vertex])
-        if (status[v.id] == NOTVISITED)
-            visit(graph, v.id, stack, status);
-    status[vertex] = FINISHED;
-    stack.push_back(vertex);
-}
-
-void dfs(Graph* graph, std::vector<int> &stack) {
-    std::vector<int> status(graph->vertex_count, NOTVISITED);
-    for (size_t i = 0; i < graph->vertex_count; i++)
-        if (status[i] == NOTVISITED)
-            visit(graph, i, stack, status);
-}
-
-// Applying the algorithm relying on dfs to get the graph's sccs
-void get_sccs(Graph* graph, std::vector<std::vector<int>> &sccs) {
-    std::vector<int> status(graph->vertex_count, NOTVISITED);
-    std::vector<int> stack;
-    dfs(graph, stack);
-    // No need to compute G^t since all edges are bi-directional, G == G^t
-    for (const auto & vertex : stack) {
-        if (status[vertex] == NOTVISITED) {
-            std::vector<int> scc;
-            visit(graph, vertex, scc, status);
-            sccs.push_back(scc);
-        }
+void max_heapify(heap_t* heap, int i) {
+    int l = left(i), r = right(i), largest = i;
+    
+    if (l < heap->size && heap->edges[l].weight > heap->edges[i].weight)
+        largest = l;
+    
+    if (r < heap->size && heap->edges[r].weight > heap->edges[largest].weight)
+        largest = r;
+    
+    if (largest != i) {
+        swap(heap, i, largest);
+        max_heapify(heap, largest);
     }
 }
 
-unsigned long int compute_graph(Graph* graph) {
+void build_max_heap(heap_t* heap) {
+    size_t size = heap->size >> 1;
+    for (size_t i = size; i > 0; i--)
+        max_heapify(heap, i);
+}
+
+void make_set(tree_node_t* x) {
+    x->p = x;
+    x->rank = 0;
+}
+
+tree_node_t* find_set(tree_node_t* x) {
+    if (x != x->p)
+        x->p = find_set(x->p);
+    return x->p;
+}
+
+void link(tree_node_t* x, tree_node_t* y) {
+    if (x->rank > y->rank) {
+        y->p = x;
+    } else {
+        x->p = y;
+        if (x->rank == y->rank)
+            y->rank++;
+    }
+}
+
+void node_union(tree_node_t* x, tree_node_t* y) {
+    link(find_set(x), find_set(y));
+}
+
+unsigned long int get_maximum_cost_spanning_tree(heap_t* heap, std::vector<tree_node_t*>& nodes) {
     unsigned long int result = 0;
     
-    if (graph->vertex_count == 0)
-        return 0;
+    for (auto & node : nodes)
+        make_set(node);
 
-    std::vector<std::vector<int>> sccs;
-    get_sccs(graph, sccs);
-
-    for (auto & vertices : sccs)
-        result += get_maximum_cost_spanning_tree(graph, vertices);
+    for (int i = 0; i < heap->size; i++) {
+        if (find_set(heap->edges[i].u) != find_set(heap->edges[i].v)) {
+            node_union(heap->edges[i].u, heap->edges[i].v);
+            result += heap->edges[i].weight;
+        }
+    }
 
     return result;
 }
 
-Graph* read_input(void) {
-    size_t edge_count;
-    Graph* graph = new Graph();
-    std::cin >> graph->vertex_count >> edge_count;
-    graph->adjacent_vertices = std::vector<std::vector<Vertex>>(graph->vertex_count);
-    for (size_t i = 0; i < edge_count; i++) {
+unsigned long int compute_graph(heap_t* heap, std::vector<tree_node_t*> &nodes) {
+    unsigned long int result = 0;
+    result += get_maximum_cost_spanning_tree(heap, nodes);
+    return result;
+}
+
+
+int main(void) {
+    int v_count, e_count;
+    std::cin >> v_count >> e_count;
+    
+    heap_t* heap = new heap_t();
+    std::vector<tree_node_t*> nodes = std::vector<tree_node_t*>(v_count, nullptr);;
+    for (int i = 0; i < e_count; i++) {
         int id1, id2;
         size_t weight;
         scanf("%d %d %ld", &id1, &id2, &weight);
-        graph->adjacent_vertices[id1-1].push_back(Vertex({.id = id2-1, .weight = weight}));
-        graph->adjacent_vertices[id2-1].push_back(Vertex({.id = id1-1, .weight = weight}));
-    }
-    return graph;
-}
+        
+        if (nodes[--id1] == nullptr)
+            nodes[id1] = new tree_node_t({.id = id1});
 
-int main(void) {
-    Graph* graph = read_input();
-    std::cout << compute_graph(graph) << std::endl;
-    delete graph;
+        if (nodes[--id2] == nullptr)
+            nodes[id2] = new tree_node_t({.id = id2});
+
+        heap->edges.push_back(edge_t({.u = nodes[id1], .v = nodes[id2], .weight = weight}));
+    }
+
+    heap->size = e_count;
+    build_max_heap(heap);
+
+    std::cout << compute_graph(heap, nodes) << std::endl;
+    
+    for (auto & node : nodes)
+        delete node;
+
+    delete heap;
 }
