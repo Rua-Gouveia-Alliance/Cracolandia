9249a8eda92a31a42ace31298d90d6efea6fcdc1
Joao Gouveia <jjtucki4@gmail.com>
2022-12-30 16:16:17 +0000

    Change from heap to radix

--- src/p2.cpp
+++ src/p2.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 struct tree_node_t {
     tree_node_t* p;
     size_t rank;
+    int id;
 };
 
 struct edge_t {
@@ -12,77 +14,10 @@ struct edge_t {
     size_t weight;
 };
 
-struct heap_t {
+struct vector_t {
     int size;
-    std::vector<edge_t*> edges;
+    std::vector<edge_t*> data;
 };
-
-static inline int parent(int i) {
-    return (i >> 1);
-}
-
-static inline int left(int i) {
-    return (i << 1);
-}
-
-static inline int right(int i) {
-    return (i << 1) + 1;
-}
-
-void swap(heap_t* heap, int i, int j) {
-    edge_t* aux = heap->edges[i];
-    heap->edges[i] = heap->edges[j];
-    heap->edges[j] = aux;
-}
-
-void max_heapify(heap_t* heap, int i) {
-    int l = left(i), r = right(i), largest = i;
-    
-    if (l < heap->size + 1 && heap->edges[l]->weight > heap->edges[i]->weight)
-        largest = l;
-    
-    if (r < heap->size + 1 && heap->edges[r]->weight > heap->edges[largest]->weight)
-        largest = r;
-    
-    if (largest != i) {
-        swap(heap, i, largest);
-        max_heapify(heap, largest);
-    }
-}
-
-void heap_increase_key(heap_t *heap, int i, size_t key) {
-    if (heap->edges[i]->weight == key)
-        return;
-
-    heap->edges[i]->weight = key;
-
-    while (i > 1 && heap->edges[parent(i)]->weight < heap->edges[i]->weight) {
-        swap(heap, i, parent(i));
-        i = parent(i);
-    }
-}
-
-void max_heap_insert(heap_t* heap, edge_t* edge) {
-    heap->size++;
-    size_t key = edge->weight;
-    edge->weight = 0;
-    heap->edges.push_back(edge);
-    heap_increase_key(heap, heap->size, key);
-}
-
-edge_t* extract_max(heap_t* heap) {
-    edge_t* max = heap->edges[1];
-    heap->edges[1] = heap->edges[heap->size];
-    heap->size--;
-    max_heapify(heap, 1);
-    return max;
-}
-
-void build_max_heap(heap_t* heap) {
-    int size = heap->size >> 1;
-    for (int i = size; i > 0; i--)
-        max_heapify(heap, i);
-}
 
 void make_set(tree_node_t* x) {
     x->p = x;
@@ -109,11 +44,48 @@ void node_union(tree_node_t* x, tree_node_t* y) {
     link(find_set(x), find_set(y));
 }
 
-size_t get_maximum_cost_spanning_tree(heap_t* heap) {
-    size_t result = 0;
+int pow10(int exp) {
+    int result = 1;
+    for (int i = 0; i < exp; i++)
+        result *= 10;
+    return result;
+}
 
-    while (heap->size > 0) {   
-        edge_t* edge = extract_max(heap);
+static inline int nth_digit(size_t num, int n) {
+    return (num / pow10(n)) % 10;
+}
+
+void counting_sort(vector_t* a, int n) {
+    std::vector<int> c(10, 0);
+    std::vector<edge_t*> b(a->size);
+    
+    for (int i = 0; i < a->size; i++)
+        c[nth_digit(a->data[i]->weight, n)]++;
+    
+    for (int i = 1; i < 10; i++)
+        c[i] += c[i-1];
+
+    for (int i = a->size - 1; i > -1; i--) {
+        b[c[nth_digit(a->data[i]->weight, n)]-1] = a->data[i];
+        c[nth_digit(a->data[i]->weight, n)]--;
+    }
+    
+    for (int i = 0; i < a->size; i++)
+        a->data[i] = b[i];
+}
+
+void radix_sort(vector_t* vector, int d_count) {
+    for (int i = 0; i < d_count; i++)
+        counting_sort(vector, i);
+}
+
+size_t get_maximum_cost_spanning_tree(vector_t* vector, int d_count) {
+    size_t result = 0;
+    
+    radix_sort(vector, d_count);
+
+    for (int i = vector->size - 1; i > -1; i--) {   
+        edge_t* edge = vector->data[i];
         if (find_set(edge->u) != find_set(edge->v)) {
             node_union(edge->u, edge->v);
             result += edge->weight;
@@ -124,21 +96,19 @@ size_t get_maximum_cost_spanning_tree(heap_t* heap) {
     return result;
 }
 
-void read_input (heap_t *heap, std::vector<tree_node_t*> &nodes) {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-
+void read_input (vector_t *vector, std::vector<tree_node_t*> &nodes, size_t& max) {
     int v_count, e_count;
 
     std::cin >> v_count >> e_count;
     for (int i = 0; i < v_count; i++) {
         nodes.push_back(new tree_node_t());
         make_set(nodes[i]);
+        nodes[i]->id = i;
     }
 
     int id1, id2;
     size_t weight, min;
-    heap->edges.push_back(nullptr);
+    vector->size = 0;
     for (int i = 0; i < e_count; i++) {
         std::cin >> id1 >> id2 >> weight;
         
@@ -147,26 +117,32 @@ void read_input (heap_t *heap, std::vector<tree_node_t*> &nodes) {
 
         if (i < v_count || weight > min) {
             edge_t* new_edge =  new edge_t({.u = nodes[id1-1], .v = nodes[id2-1], .weight = weight});
-            max_heap_insert(heap, new_edge);
+            vector->data.push_back(new_edge);
+            vector->size++;
             
             if (weight < min)
                 min = weight;
+            if (weight > max)
+                max = weight;
         }
     }
 }
 
 int main(void) {
-    heap_t* heap = new heap_t();
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    size_t max = 0;
+    vector_t* vector = new vector_t();
     std::vector<tree_node_t*> nodes;
-    heap->size = 0;
-    read_input(heap, nodes);
+    read_input(vector, nodes, max);
     
-    std::cout << get_maximum_cost_spanning_tree(heap) << std::endl;
+    std::cout << get_maximum_cost_spanning_tree(vector, floor(log10(max)) + 1) << std::endl;
     
     for (auto & node : nodes)
         delete node;
 
-    delete heap;
+    delete vector;
     
     return 0;
 }
--- tests/generate_test.py
+++ tests/generate_test.py
@@ -7,7 +7,7 @@ with open("generated_test.in", "w") as test:
     for i in range(1, vertices + 1):
         for j in range(1, vertices + 1):
             if (j != i):
-                test.write(str(i) + " " + str(j) + " 1\n")
+                test.write(str(i) + " " + str(j) + " " + str(i*j+1) + "\n")
 
     with open("generated_test.out", "w") as result:
         result.write(str(vertices-1) + "\n")
